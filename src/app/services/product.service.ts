f6407dcf4358812cd990cf07b26440c724d38bc2
Santiago Caballero <saantyc12.caballero@gmail.com>
2023-02-09 00:03:02 -0300

    change styles

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,28 +3,29 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { FoodProduct } from '../models/product.model';
 import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-  // private apiUrl : string = 'https://backend-angular-food-store.onrender.com'
+  // private apiUrl: string = 'https://backend-angular-food-store.onrender.com';
   private apiUrl: string = 'http://localhost:3001'
-  constructor(
-    private http: HttpClient
-  ) { }
-  getAllProducts(){
-    return this.http.get<FoodProduct[]>(this.apiUrl + '/foods')
+  constructor(private http: HttpClient) {}
+  getAllProducts() {
+    return this.http.get<FoodProduct[]>(this.apiUrl + '/foods');
   }
-  getProductDetail(id: string | null){
-    return this.http.get<FoodProduct>(this.apiUrl + '/foods/' + id)
+  getProductDetail(id: string | null) {
+    return this.http.get<FoodProduct>(this.apiUrl + '/foods/' + id);
   }
-  getProductsByType(type:string){
-    return this.http.get<FoodProduct[]>(this.apiUrl + '/foods/filter?type=' + type)
+  getProductsByType(type: string) {
+    return this.http.get<FoodProduct[]>(
+      this.apiUrl + '/foods/filter?type=' + type
+    );
   }
-  createNewFood(data: FoodProduct){
-    return this.http.post<FoodProduct>(this.apiUrl + '/foods', data)
+  createNewFood(data: FoodProduct) {
+    return this.http.post<FoodProduct>(this.apiUrl + '/foods', data);
   }
-  getProductByName(value:string){
-    return this.http.get<FoodProduct[]>(this.apiUrl + '/foods/byname?name=' + value)
+  getProductByName(value: string) {
+    return this.http.get<FoodProduct[]>(
+      this.apiUrl + '/foods/byname?name=' + value
+    );
   }
-
 }
